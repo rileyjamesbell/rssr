33d4da886bfc4c20b4258debc10ad69d15926622
lolico <46867542+rileyjamesbell@users.noreply.github.com>
2020-10-27 05:47:56 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,8 @@
 #!/bin/sh
 
-#Makefiles are overrated
+CC="gcc"
+MAIN="nciout.c"
+OUT="nciout"
+LIBS=$(pkg-config --libs ncursesw)
 
-nciout(){
-	CC="gcc"
-	MAIN="nciout.c"
-	OUT="nciout"
-	LIBS=$(pkg-config --libs ncursesw)
-
-	$CC $MAIN $LIBS -o $OUT
-}
-
-case $1 in
-	"all"|"")
-		echo "Building nciout"
-		nciout
-		;;
-	"nciout")
-		nciout
-		;;
-	*)
-		echo "Specify which program to build"
-		return 1
-		;;
-esac
+$CC $MAIN $LIBS -o $OUT
